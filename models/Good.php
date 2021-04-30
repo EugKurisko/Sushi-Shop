6bfc02c2598085e3158c521679b76ecbc38cc73e
Eugen <gram25042002@gmail.com>
2021-04-30 19:08:13 +0300

    added goods  caching on the page

--- models/Good.php
+++ models/Good.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use yii\db\ActiveRecord;
+use Yii;
 
 class Good extends ActiveRecord
 {
@@ -13,7 +14,11 @@ class Good extends ActiveRecord
 
     public function getAllGoods()
     {
-        $goods = Good::find()->asArray()->all(); //go to db and get everything
+        $goods = Yii::$app->cache->get('goods'); //check info in cache
+        if (!$goods) {
+            $goods = Good::find()->asArray()->all(); //go to db and get everything
+            Yii::$app->cache->set('goods', $goods, 30); //set cache (key, var(what is needed to be cached), time)
+        }
         return $goods;
     }
 }
