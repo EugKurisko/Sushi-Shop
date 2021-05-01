bb72d86cd2faacee276a2f15927209a63d619e5a
Eugen <gram25042002@gmail.com>
2021-05-01 14:35:07 +0300

    get data from db for categories (with caching) and for searching

--- models/Good.php
+++ models/Good.php
@@ -21,4 +21,21 @@ class Good extends ActiveRecord
         }
         return $goods;
     }
+
+    public function getGoodsCategories(string $category)
+    {
+        $catGoods = Yii::$app->cache->get("catGoods[$category]");
+        if (!$catGoods) {
+            $catGoods = Good::find()->where(['category' => $category])->asArray()->all();
+            Yii::$app->cache->set("catGoods[$category]", $catGoods, 30);
+        }
+        return $catGoods;
+    }
+    public function getSearchResults(string $search)
+    {
+        $searchResult = Good::find()->where(['like', 'name', $search])->asArray()->all();
+        //like == similar
+        //name -> where to find, search -> what to find
+        return $searchResult;
+    }
 }
