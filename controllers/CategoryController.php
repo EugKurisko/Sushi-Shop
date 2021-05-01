1cfcc26d1c7d50cc4258c31d7acfa164b0da0ed8
Eugen <gram25042002@gmail.com>
2021-05-01 14:33:57 +0300

    added view - display goods by category and search - display goods by key word

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\models\Good;
 use yii\web\Controller;
+use yii;
 
 class CategoryController extends Controller
 {
@@ -12,5 +13,20 @@ class CategoryController extends Controller
         $goods = new Good(); //create instance of good model
         $goods = $goods->getAllGoods(); //rewrite var to get everything grom db
         return $this->render('index', compact('goods'));
+    }
+
+    public function actionView($category)
+    {
+        $catGoods = new Good();
+        $catGoods = $catGoods->getGoodsCategories($category);
+        return $this->render('view', compact('catGoods'));
+    }
+
+    public function actionSearch()
+    {
+        $search = htmlspecialchars(Yii::$app->request->get('search')); //get data from input with name 'search' passed as $_GET 
+        $goods = new Good();
+        $goods = $goods->getSearchResults($search);
+        return $this->render('search', compact('goods', 'search'));
     }
 }
