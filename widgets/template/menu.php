651cf20cd17c120c7fb234f94cf6fdde67b67966
Eugen <gram25042002@gmail.com>
2021-05-01 14:39:14 +0300

    redirect to page of selected category

--- widgets/template/menu.php
+++ widgets/template/menu.php
@@ -1,8 +1,13 @@
+<?php
+
+use yii\helpers\Url;
+?>
+
 <div class="container">
     <nav class="nav nav-menu">
-        <a class="nav-link active" href="#">Всё меню</a>
+        <a class="nav-link active" href="<?php echo Yii::$app->request->baseUrl ?>" . "../category/index.php">Всё меню</a>
         <?php foreach ($data as $cat) : ?>
-            <a class="nav-link" href="#"><?= $cat['browser_name'] ?></a>
+            <a class="nav-link" href="<?= URL::to(['category/view', 'category' => $cat['cat_name']]) ?>"><?= $cat['browser_name'] ?></a>
         <?php endforeach; ?>
     </nav>
 </div>
