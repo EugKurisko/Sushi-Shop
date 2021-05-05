4488aa2bc9ce4ec057a79cbb469c8659b5648d92
Eugen <gram25042002@gmail.com>
2021-05-05 18:41:34 +0300

    added ajax logic for adding/deleting from cart, opening form

--- web/js/main.js
+++ web/js/main.js
@@ -1,15 +1,22 @@
-// $('.cart').on('click', function () {
-//     $('#cart').modal('show');
-// });
-
-//var urls = ['cart/add', '/cart/add'];
-
-//'cart/open', '../cart/open', 
+$('.modal-content').on('click', '.btn-next', function () {
+    $.ajax({
+        url: 'cart/order', //a.attr('href'),//'~/cart/open',
+        type: 'GET',
+        success: function (res) {
+            $('#order .modal-content').html(res);
+            $('#cart').modal('hide');
+            $('#order').modal('show');
+        },
+        error: function () {
+            alert("not ok");
+        }
+    })
+});
 
 function openCart(event) {
     event.preventDefault();
     $.ajax({
-        url: 'cart/open',
+        url: 'cart/open', //a.attr('href'),//'~/cart/open',
         type: 'GET',
         success: function (res) {
             $('#cart .modal-content').html(res);
@@ -21,61 +28,60 @@ function openCart(event) {
     })
 }
 
-// function openCart(event) {
-//     event.preventDefault();
-//     $.ajax({
-//         url: '/cart/open',
-//         type: 'GET',
-//         success: function (res) {
-//             $('#cart .modal-content').html(res);
-//             $('#cart').modal('show');
-//         },
-//         error: function () {
-//             alert("not ok");
-//         }
-//     })
-// }
-// $('.product-button__add').on('click', function (event) {
-//     event.preventDefault();
-//     let name = $(this).data('name');
-//     console.log(name);
-
-//     $.ajax({
-//         url: '/cart/add',
-//         data: { name: name },
-//         type: 'GET',
-//         success: function (res) {
-//             $('#cart .modal-content').html(res);
-//         },
-//         error: function () {
-//             alert("not ok");
-//         }
-//     })
-// })
+function clearCart(event) {
+    if (confirm('Точно очистить корзину?')) {
+        event.preventDefault();
+        $.ajax({
+            url: 'cart/clear', //a.attr('href'),//'~/cart/open',
+            type: 'GET',
+            success: function (res) {
+                $('#cart .modal-content').html(res);
+                $('.menu-quantity').html('(0)');
+            },
+            error: function () {
+                alert("not ok");
+            }
+        })
+    }
+}
 
 $('.product-button__add').on('click', function (event) {
     event.preventDefault();
     let name = $(this).data('name');
     console.log(name);
-    // $.each(urls, function (i, u) {
-    //     $.ajax(u,
-    //         {
-    //             type: 'GET',
-    //             function(res) {
-    //                 $('#cart .modal-content').html(res);
-    //             },
-    //             error: function () {
-    //                 alert("not ok");
-    //             }
-    //         }
-    //     );
-    // });
     $.ajax({
-        url: 'cart/add',
+        url: 'cart/add', //a.attr('href'),//'~/cart/add',
         data: { name: name },
         type: 'GET',
         success: function (res) {
             $('#cart .modal-content').html(res);
+            $('.menu-quantity').html('(' + $('.total-quantity').html() + ')');
+        },
+        error: function () {
+            alert("not ok");
+        }
+    })
+})
+
+$('.modal-content').on('click', '.btn-close', function () {//refer to parent class. and using parent refer to child class
+    //use when something generates dynamically
+    $('#cart').modal('hide');
+})
+
+$('.modal-content').on('click', '.delete', function () {//refer to parent class. and using parent refer to child class
+    let id = $(this).data('id');
+    //console.log(id);
+    $.ajax({
+        url: 'cart/delete', //a.attr('href'),//'~/cart/add',
+        data: { id: id },
+        type: 'GET',
+        success: function (res) {
+            $('#cart .modal-content').html(res);
+            if ($('.total-quantity').html()) {
+                $('.menu-quantity').html('(' + $('.total-quantity').html() + ')');
+            } else {
+                $('.menu-quantity').html('(0)');
+            }
         },
         error: function () {
             alert("not ok");
