2a32aadfca9a30d12e1c1de158d7d10f24f88b42
Eugen <gram25042002@gmail.com>
2021-05-05 18:35:37 +0300

    fixed bug with calculation

--- views/cart/cart.php
+++ views/cart/cart.php
@@ -4,7 +4,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
 <?php if (!$session['cart']) : ?>
-    <h2>Товаров нет</h2>
+    <h3>Товаров нет</h3>
+    <button type="button" class="btn btn-secondary btn-close" style="width: 200px; height: 50px;">Начать покупки</button>
 <?php else : ?>
 
     <body>
@@ -23,39 +24,31 @@
             </thead>
 
             <tbody>
-                <?php
-                $totalSum = 0;
-                $totalQuantity = 0;
-                ?>
                 <?php foreach ($session['cart'] as $id => $good) : ?>
                     <tr>
                         <td style="vertical-align: middle" width="150"><img src="<?php echo Yii::$app->request->baseUrl ?>/img/<?= $good['img'] ?>" alt="img"></td>
                         <td style="vertical-align: middle"><?= $good['name'] ?></td>
                         <td style="vertical-align: middle"><?= $good['quantity'] ?></td>
                         <td style="vertical-align: middle"><?= $good['price'] * $good['quantity'] ?> гривен</td>
-                        <td class="delete" style="text-align: center; cursor: pointer; vertical-align: middle; color: red">
+                        <td class="delete" data-id="<?= $id ?>" style="text-align: center; cursor: pointer; vertical-align: middle; color: red">
                             <span>✖</span>
                         </td>
                     </tr>
-                    <?php
-                    $totalSum += $good['price'] * $good['quantity'];
-                    $totalQuantity += $good['quantity'];
-                    ?>
                 <?php endforeach; ?>
                 <tr style="border-top: 4px solid black">
                     <td colspan="4">Всего товаров</td>
-                    <td class="total-quantity"><?= $totalQuantity ?></td>
+                    <td class="total-quantity"><?= $session['cart.totalQuantity'] ?></td>
                 </tr>
                 <tr>
                     <td colspan="4">На сумму </td>
-                    <td style="font-weight: 700"><?= $totalSum ?></td>
+                    <td style="font-weight: 700"><?= $session['cart.totalSum'] ?></td>
                 </tr>
             </tbody>
 
         </table>
 
         <div class="modal-buttons" style="display: flex; padding: 15px; justify-content: space-around">
-            <button type="button" class="btn btn-danger">Очистить корзину</button>
+            <button type="button" class="btn btn-danger" onclick="clearCart(event)">Очистить корзину</button>
             <button type="button" class="btn btn-secondary btn-close">Продолжить покупки</button>
             <button type="button" class="btn btn-success btn-next">Оформить заказ</button>
         </div>
