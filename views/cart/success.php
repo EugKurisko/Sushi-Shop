4f84824d3f844467c3b7e3c91cc6fea1652d7365
Eugen <gram25042002@gmail.com>
2021-05-08 15:09:43 +0300

    success page

--- views/cart/success.php
+++ views/cart/success.php
@@ -1,1 +1,2 @@
-<h1>Спасибо за заказ</h1>
+<h1>Ваш заказ под номером <?= $currentId ?> принят</h1>
+<a href="<?php echo Yii::$app->request->baseUrl ?>" . "../category/index.php" class="btn btn-success">Вернуться на главную</a>
