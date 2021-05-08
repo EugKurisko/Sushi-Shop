49c8f1900591c1f70459969248e187db754ea4f5
Eugen <gram25042002@gmail.com>
2021-05-08 15:08:30 +0300

    active class for categories

--- widgets/template/menu.php
+++ widgets/template/menu.php
@@ -5,9 +5,9 @@ use yii\helpers\Url;
 
 <div class="container">
     <nav class="nav nav-menu">
-        <a class="nav-link active" href="<?php echo Yii::$app->request->baseUrl ?>" . "../category/index.php">Всё меню</a>
+        <a class="nav-link" href="<?php echo Yii::$app->request->baseUrl ?>" . "../category/index.php">Всё меню</a>
         <?php foreach ($data as $cat) : ?>
-            <a class="nav-link" href="<?= URL::to(['category/view', 'category' => $cat['cat_name']]) ?>"><?= $cat['browser_name'] ?></a>
+            <a class="nav-link" data-id="<?= $cat['cat_name'] ?>" href="<?= URL::to(['category/view', 'category' => $cat['cat_name']]) ?>"><?= $cat['browser_name'] ?></a>
         <?php endforeach; ?>
     </nav>
 </div>
